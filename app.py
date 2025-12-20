547e1c832ce4b42bc0f4312d8631221ba815d03b
mrinmoyChakraborty-mrinox <chakrabortymrinmoy27011@gmail.com>
2025-12-20 12:53:55 +0530

    still fixing....

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ def login():
 
     # Check / create user document in Firestore (no password)
     user_data = firebase_service.get_user(email)
-    if not user:
+    if not user_data:
         firebase_service.add_user(email=email, username=name)
         user_data = {"email": email, "username": name, "photo_url": decoded.get("picture", "https://ik.imagekit.io/RemediRX/pngwing.com.png?updatedAt=1764494288724"),"fcm_enabled": False}
 
